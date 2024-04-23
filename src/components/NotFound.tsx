a09fe5b0f7bfe7de70e95648b6ec17cb85d9e524
Zekumoru Dragonhart <57686690+Zekumoru@users.noreply.github.com>
2024-04-23 16:47:38 +0200

    Create not found page

---
+++ src/components/NotFound.tsx
@@ -0,0 +1,12 @@
+const NotFound = () => {
+  return (
+    <>
+      <h1 className="text-4xl">404 Not Found</h1>
+      <p className="my-4">
+        The resource you're looking for is either unavailable or does not exist!
+      </p>
+    </>
+  );
+};
+
+export default NotFound;
