2bcd5f4ce30c4ccbfe58459f0eed19f9e149deaf
Zekumoru Dragonhart <57686690+Zekumoru@users.noreply.github.com>
2024-04-23 16:47:45 +0200

    Create home page

---
+++ src/components/Home.tsx
@@ -0,0 +1,12 @@
+const Home = () => {
+  return (
+    <>
+      <h1 className="text-4xl">Welcome!</h1>
+      <p className="my-4">
+        This is a simple web application to play some Bible games!
+      </p>
+    </>
+  );
+};
+
+export default Home;
