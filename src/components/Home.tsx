f5ef504da534fcbd58ed5ceeb33da298178234a1
Zekumoru Dragonhart <57686690+Zekumoru@users.noreply.github.com>
2024-04-23 16:56:29 +0200

    Add h1 base style

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,7 @@
 const Home = () => {
   return (
     <>
-      <h1 className="text-4xl">Welcome!</h1>
+      <h1>Welcome!</h1>
       <p className="my-4">
         This is a simple web application to play some Bible games!
       </p>
