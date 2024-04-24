ab420bc6cc8a0e2d70af99381e08df1213ddc28f
Zekumoru Dragonhart <57686690+Zekumoru@users.noreply.github.com>
2024-04-24 07:23:12 +0200

    List cards

--- src/components/shuffler/Cards.tsx
+++ src/components/shuffler/Cards.tsx
@@ -1,10 +1,12 @@
 import { useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import CardModal from './CardModal';
+import useCardsQuery from './hooks/useCardsQuery';
 
 const Cards = () => {
   const modalRef = useRef<HTMLDialogElement>(null);
   const navigate = useNavigate();
+  const [cards, refetchCards, error, isFetching] = useCardsQuery();
 
   return (
     <>
@@ -27,14 +29,34 @@ const Cards = () => {
       </div>
 
       <h2 className="mb-4 mt-6">List of cards</h2>
+      {isFetching ? (
+        <div>Loading cards...</div>
+      ) : error?.message ? (
+        <div>Could not get cards from the server.</div>
+      ) : (
+        <ul className="flex flex-col gap-4">
+          {cards.map(({ name }, index) => (
+            <li key={name} className="card bg-neutral shadow-xl">
+              <div className="card-body flex-row items-center justify-between p-4">
+                <div className="flex gap-2 text-lg">
+                  <div>{index + 1}</div>
+                  <div>{name}</div>
+                </div>
+                <div className="card-actions">
+                  <button className="btn">Edit</button>
+                  <button className="btn btn-error">Delete</button>
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      )}
 
       <CardModal
         title="Add New Card"
         type="create"
         ref={modalRef}
-        onSuccess={() => {
-          modalRef.current?.close();
-        }}
+        onSuccess={refetchCards}
       />
     </>
   );
