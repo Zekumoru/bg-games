f151efb13376397a215d8a20ab2c93fc4c2e1295
Zekumoru Dragonhart <57686690+Zekumoru@users.noreply.github.com>
2024-04-24 07:21:27 +0200

    Use imperative handle hook for card modal ref

--- src/components/shuffler/CardModal.tsx
+++ src/components/shuffler/CardModal.tsx
@@ -1,9 +1,15 @@
-import { forwardRef, useEffect, useState } from 'react';
+import {
+  forwardRef,
+  useEffect,
+  useImperativeHandle,
+  useRef,
+  useState,
+} from 'react';
 import useCardMutation from './hooks/useCardMutation';
 import TextInput from '../form/TextInput';
 
 const CardModal = forwardRef<
-  HTMLDialogElement,
+  HTMLDialogElement | null,
   {
     title: string;
     type: 'create' | 'update';
@@ -11,8 +17,14 @@ const CardModal = forwardRef<
     onSuccess?: () => void;
   }
 >(({ title, type, initialName, onSuccess }, ref) => {
+  const modalRef = useRef<HTMLDialogElement | null>(null);
   const [name, setName] = useState(type === 'update' ? initialName ?? '' : '');
   const [mutateCard, status, error, isPending] = useCardMutation();
+
+  useImperativeHandle<HTMLDialogElement | null, HTMLDialogElement | null>(
+    ref,
+    () => modalRef.current,
+  );
 
   const addCard = () => {
     mutateCard({ type: 'create', data: { name } });
@@ -22,12 +34,18 @@ const CardModal = forwardRef<
     if (!status) return;
 
     if (status.status >= 200 && status.status < 300) {
+      modalRef.current?.close();
+      setName('');
       onSuccess?.();
     }
   }, [status, onSuccess]);
 
+  const handleSubmit = () => {
+    if (type === 'create') addCard();
+  };
+
   return (
-    <dialog className="modal" ref={ref}>
+    <dialog className="modal" ref={modalRef}>
       {isPending && (
         <div className="fixed left-0 top-0 z-50 grid h-screen w-screen place-content-center">
           <div className="absolute bottom-0 top-0 h-screen w-screen bg-neutral-800 opacity-40"></div>
@@ -42,12 +60,25 @@ const CardModal = forwardRef<
           value={name}
           setValue={setName}
           errMessage={error?.errors.name?.msg}
+          onEnter={handleSubmit}
         />
 
         <div className="modal-action">
-          <form className="flex gap-2" method="dialog">
-            <button className="btn btn-ghost">Close</button>
-            <button className="btn btn-primary" type="button" onClick={addCard}>
+          <form
+            className="flex gap-2"
+            onSubmit={(e) => {
+              e.preventDefault();
+              handleSubmit();
+            }}
+          >
+            <button
+              className="btn btn-ghost"
+              type="button"
+              onClick={() => modalRef.current?.close()}
+            >
+              Close
+            </button>
+            <button className="btn btn-primary" type="submit">
               Add card
             </button>
           </form>
