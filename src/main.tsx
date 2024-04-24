04ba76d2463030a009bea8275f477d7a489a7430
Zekumoru Dragonhart <57686690+Zekumoru@users.noreply.github.com>
2024-04-24 16:17:39 +0200

    Hide tanstack devtools

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import App from './App.tsx';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 export const queryClient = new QueryClient();
 
@@ -13,7 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <BrowserRouter basename="/bg-games">
       <QueryClientProvider client={queryClient}>
         <App />
-        <ReactQueryDevtools initialIsOpen={false} />
+        {/* <ReactQueryDevtools initialIsOpen={false} /> */}
       </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>,
